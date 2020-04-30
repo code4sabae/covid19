1b74e40686ced856a40251238fb4abe0bac0a309
Taisuke Fukuno <fukuno@jig.jp>
2020-04-30 19:43:03 +0900

    データ更新（厚労省）

--- node/covid19kumamoto.mjs
+++ node/covid19kumamoto.mjs
@@ -1,19 +1,18 @@
 import cheerio from 'cheerio'
 import util from './util.js'
-import fs from 'fs'
 import fetch from 'node-fetch'
 import htmlparser from './htmlparser.mjs'
 import dateparser from './dateparser.mjs'
 import covid19fast from './covid19fast.mjs'
 
-const main = async function() {
-  const url_opendata = 'https://www.pref.kumamoto.jp/kiji_22038.html'
-  const pref = "Kumamoto"
+const main = async function () {
+  const urlOpendata = 'https://www.pref.kumamoto.jp/kiji_22038.html'
+  const pref = 'Kumamoto'
 
-  const lpref = pref.toLowerCase()
-  //const path = '../data/covid19' + lpref + '/'
-  const path = 'temp/covid19' + lpref + '/'
-  const fn = path + "test.html"
+  // const lpref = pref.toLowerCase()
+  // const path = '../data/covid19' + lpref + '/'
+  // const path = 'temp/covid19' + lpref + '/'
+  // const fn = path + "test.html"
 
   /*
   const list = []
@@ -23,15 +22,15 @@ const main = async function() {
   }
   */
 
-  //const html = fs.readFileSync(fn, 'utf-8')
-  const html = await (await fetch(url_opendata)).text()
-  //fs.writeFileSync(fn, html, 'utf-8')
-  //console.log(html)
+  // const html = fs.readFileSync(fn, 'utf-8')
+  const html = await (await fetch(urlOpendata)).text()
+  // fs.writeFileSync(fn, html, 'utf-8')
+  // console.log(html)
 
   const dom = cheerio.load(html)
   const tbls = htmlparser.parseTables(dom)
-  //console.log(tbls)
-  
+  // console.log(tbls)
+
   /*
   let cnt = 0
   for (const tbl of tbls) {
@@ -42,17 +41,17 @@ const main = async function() {
   const json = util.csv2json(tbls[1])
   for (const d of json) {
     const name = d['データ名称']
-    if (name.indexOf('陽性患者属性') == -1)
+    if (name.indexOf('陽性患者属性') === -1) {
       continue
+    }
     const dt = dateparser.parseDate(name)
     console.log(dt)
     const csvurl = d['ファイル（CSV形式）'].split(' ')[0]
     console.log(csvurl)
-    const res = await covid19fast.makeData(pref, csvurl, url_opendata)
+    const res = await covid19fast.makeData(pref, csvurl, urlOpendata)
     console.log(res)
     break
   }
-  
 }
 if (process.argv[1].endsWith('/covid19kumamoto.mjs')) {
   main()
---
+++ node/makeogp.mjs
@@ -0,0 +1,63 @@
+import fs from 'fs'
+import util from './util.mjs'
+import webdriver from 'selenium-webdriver'
+import Jimp from 'jimp'
+import cheerio from 'cheerio'
+
+const { Builder, By, until } = webdriver
+
+const capabilities = webdriver.Capabilities.chrome()
+capabilities.set('chromeOptions', {
+  args: [
+    '--headless',
+    '--no-sandbox',
+    '--disable-gpu',
+    '--window-size=820,640' // 効かない
+  ]
+})
+
+const saveScreenShot = async function (fn) {
+  const driver = await new Builder().withCapabilities(capabilities).build()
+
+  //await driver.get('https://www.stopcovid19.jp/#ja')
+  await driver.get('http://localhost:8888/fukunojigjp/app/covid19/#ja')
+
+  // await driver.wait(until.elementLocated(By.id('qrcode')), 10000)
+  await util.sleep(5000)
+
+  const base64 = await driver.takeScreenshot()
+  const buffer = Buffer.from(base64, 'base64')
+  fs.writeFileSync(fn, buffer)
+
+  driver.quit()
+}
+
+const resizeImage = async function (fn) {
+  const dw = 1220
+  const dh = 640
+
+  const png = await Jimp.read(fn)
+  const h = (dw / png.bitmap.width * png.bitmap.height) >> 0
+  png.resize(dw, h)
+  png.crop(0, 0, dw, dh)
+
+  const path = '../ogp/'
+  const dstfn = 'covid19japan_ogp_' + util.getYMDHMS() + '.png'
+  await png.write(path + dstfn)
+  console.log('write ' + dstfn)
+  return dstfn
+}
+const editOGP = function (fn) {
+  const srcfn = '../index.html'
+  let html = fs.readFileSync(srcfn, 'utf-8')
+  html = html.replace(/<meta property="og:image"\s\scontent=".+"\/>/, `<meta property="og:image"  content="https://www.stopcovid19.jp/ogp/${fn}"/>`)
+  html = html.replace(/<meta name="twitter:image" content=".+"\/>/, `<meta name="twitter:image" content="https://www.stopcovid19.jp/ogp/${fn}"/>`)
+  fs.writeFileSync(srcfn, html, 'utf-8')
+}
+const main = async function () {
+  const fn = 'screenshot.png'
+  await saveScreenShot(fn)
+  const dstfn = await resizeImage(fn)
+  editOGP(dstfn)
+}
+main()
