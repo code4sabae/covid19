90e9f5123de09e7f6eb2774a8d0bad4df961b181
Taisuke Fukuno <fukuno@jig.jp>
2020-05-02 22:55:52 +0900

    富山、対応方法を変更（zipを使わない形へ）

--- node/covid19toyama.mjs
+++ node/covid19toyama.mjs
@@ -44,6 +44,7 @@ const fetchAndSave = async function (url) {
 
 const main = async function () {
   const pref = 'Toyama'
+  /*
   const url = 'http://opendata.pref.toyama.jp/dataset/covid19/resource/de742c2d-8471-496a-9ff1-55d4afd69109'
 
   const html = await (await fetch(url)).text()
@@ -62,6 +63,11 @@ const main = async function () {
   await cmd.cmd(`unzip -o ${fn} -d temp`)
 
   const scsv = util.removeBOM(fs.readFileSync('temp/data/toyama_counts.csv', 'utf-8'))
+  */
+  const url = 'http://opendata.pref.toyama.jp/dataset/covid19/resource/85d4c612-c174-4f73-be63-f8512d25361a'
+  const urlOpendata = 'http://opendata.pref.toyama.jp/files/covid19/20200403/toyama_counts.csv'
+  const scsv = await (await fetch(urlOpendata)).text()
+
   const csv = util.decodeCSV(scsv)
   console.log(csv)
 
