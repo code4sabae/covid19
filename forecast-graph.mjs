37f63bc06741767f0b0a8b2d0aef487c76224a9d
Taisuke Fukuno <fukuno@jig.jp>
2020-12-02 07:36:59 +0900

    update data

--- forecast-graph.mjs
+++ forecast-graph.mjs
@@ -1,6 +1,7 @@
 import Chart from "https://code4sabae.github.io/kafumon/lib/Chart.mjs";
 import util from "https://taisukef.github.io/util/util.mjs";
 import { CSV } from "https://code4sabae.github.io/js/CSV.js";
+import { fix0 } from "https://code4sabae.github.io/js/fix0.js";
 
 const main = async (parent) => {
   const atts = getAttributes(parent);
@@ -70,8 +71,8 @@ const makeGraph = async (json, prefcode, parent) => {
     data: {
       labels: date,
       datasets: [
-        { type: "line", label: "入院治療を要する者", data: data_c, borderColor: 'rgb(80, 80, 205)', fill: false, lineTension: 0, yAxisID: "yr" },
-        { type: "line", label: "PCR 検査陽性者数", data: data1, borderColor: 'rgb(255, 99, 132)', fill: false, lineTension: 0, yAxisID: "yl" },
+        { type: "line", label: "PCR 検査陽性者数（予測）", data: data1, borderColor: 'rgb(255, 99, 132)', fill: false, lineTension: 0, yAxisID: "yl" },
+        { type: "line", label: "入院治療を要する者（予測）", data: data_c, borderColor: 'rgb(80, 80, 205)', fill: false, lineTension: 0, yAxisID: "yr" },
         // { type: "line", label: "死亡者数", data: data_dd, borderColor: 'rgb(10, 10, 12)', fill: false, lineTension: 0, yAxisID: "yl" },
         //{ type: "line", label: "死亡者数", data: data_d, borderColor: 'rgb(10, 10, 12)', fill: false, lineTension: 0, yAxisID: "yr" },
         // { type: "bar", label: "PCR 検査実施件数", hidden: true, data: data2, backgroundColor: 'rgb(99, 255, 132)', fill: false, lineTension: 0, yAxisID: "yr" }
@@ -90,12 +91,12 @@ const makeGraph = async (json, prefcode, parent) => {
     }
   };
   if (atts.date) {
-    const actual = await getActualData(atts["view-pref"]);
+    const actual = await getActualData(atts["view-pref"], date.map(stdDate));
     console.log(actual);
-    config.data.datasets.push({type: "bar", label: "入院を要する者（実測）", data: actual, yAxisID: "yr" });
+    config.data.datasets.push({type: "bar", label: "入院を要する者（結果）", backgroundColor: 'rgb(80, 80, 205, .2)', data: actual, yAxisID: "yr" });
   }
 
-  const title = "COVID-19 " + name + "のPCR検査陽性者数、入院者数 予測値";
+  const title = "COVID-19 " + name + "のPCR検査陽性者数と入院者数 " + (atts.date ? atts.date + "時点 予測値と結果" : "予測値");
   const h2 = document.createElement("h2");
   h2.textContent = title;
   h2.style.textAlign = "center";
@@ -115,24 +116,42 @@ const makeGraph = async (json, prefcode, parent) => {
 
   if (atts["view-src"]) {
     const div = document.createElement("div");
+    div.className = "src";
     div.style.textAlign = "center";
-    div.innerHTML = "データ出典：<a href='https://datastudio.google.com/reporting/8224d512-a76e-4d38-91c1-935ba119eb8f/page/4KwoB'>Japan: COVID-19 Public Forecasts › 日本語 - Japan COVID-19 Forecast Dashboard</a>";
+    div.innerHTML = "データ出典: <a href='https://datastudio.google.com/reporting/8224d512-a76e-4d38-91c1-935ba119eb8f/page/4KwoB'>Japan: COVID-19 Public Forecasts › 日本語 - Japan COVID-19 Forecast Dashboard by Google</a>";
+    if (atts.date) {
+      div.innerHTML += "<br>データ出典: <a href='https://www.mhlw.go.jp/stf/covid-19/open-data.html'>新型コロナウイルス感染症 入院治療等を要する者の数別 - 厚労省オープンデータ</a>";
+    }
     parent.appendChild(div);
   }
 };
-const getActualData = async (pref) => {
+const stdDate = (dt) => {
+  const n = dt.match(/(\d+)\/(\d+)/);
+  if (!n) {
+    return null;
+  }
+  const d = new Date();
+  const month = n[1];
+  const day = n[2];
+  const year = d.getFullYear() + (month < 8 ? 1 : 0);
+  return year + "-" + fix0(month, 2) + "-" + fix0(day, 2);
+};
+const getActualData = async (pref, date) => {
 	const fn = `data/covid19japan/pref/${pref}.csv`;
-	const json = await CSV.fetch(fn);
+	const json = CSV.toJSON(await CSV.fetch(fn));
 	console.log(fn, json);
 
-  const date = [];
   const data1 = [];
   const data2 = [];
   const data_c = [];
 	const data_d = [];
 	let bkis = null;
-  for (const d of json) {
-		date.push(d.date);
+  for (const dt of date) {
+    const d = json.find((d) => d.date == dt);
+    if (!d) {
+      continue;
+    }
+    console.log(dt, d);
 		data_c.push(d.ncurrentpatients);
 		data_d.push(d.ndeaths);
 		if (d.ninspections === undefined) {
