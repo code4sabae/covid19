0b84dc219ab87c24d68cbd08ca6856e83a5d745f
Taisuke Fukuno <fukuno@jig.jp>
2021-08-31 22:26:17 +0900

    update data

--- node/autoupdate.sh
+++ node/autoupdate.sh
@@ -2,7 +2,14 @@
 #do
   node makedata.js
   node covid19fast.mjs
-  deno run -A covid19forecast.deno.js 
+  deno run -A covid19forecast.deno.js
+  sh update_covid19vaccine.sh
+  sh update_fdma.sh
+  node covid19japan.mjs
+  node covid19mhlw.mjs
+  node covid19trend.mjs
+  node makeogp.mjs
+  
 #  git add ../data
 #  git commit -m 'update data'
 #  git push
